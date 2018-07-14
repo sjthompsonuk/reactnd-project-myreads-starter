16fd8464872b7bbe2c99b4fde4159bc593235096
Sam Thompson <noreply@gmail.com>
2018-07-15 01:36:09 +0200

    Add React Router functions

---
+++ src/AllShelves.js
@@ -0,0 +1,33 @@
+import React,{Component} from 'react'
+import Bookshelf from "./Bookshelf"
+import { Route } from 'react-router-dom'
+import { Link } from "react-router-dom"
+
+class AllShelves extends Component{
+
+  render() {
+    return (
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              {/*Shelf removed*/}
+              <Bookshelf books={this.props.books} category="currentlyReading" catTitle="Currently Reading" updateBooks={this.props.updateBooks}/>
+              {/*Shelf removed*/}
+              <Bookshelf books={this.props.books} category="wantToRead" catTitle="Want to Read" updateBooks={this.props.updateBooks}/>
+              {/*Shelf removed*/}
+              <Bookshelf books={this.props.books} category="read" catTitle="Read" updateBooks={this.props.updateBooks}/>
+            </div>
+          </div>
+
+          <div className="open-search">
+              <Link to="/search">Add a book</Link>
+          </div>
+
+        </div>
+    )
+  }
+}
+export default AllShelves
--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-//Need to add react routing later TODO
+import { Route } from 'react-router-dom'
+import { Link } from "react-router-dom"
 import * as BooksAPI from "./BooksAPI"
 import './App.css'
 import Bookshelf from './Bookshelf'
+import BookSearch from './BookSearch'
+import AllShelves from './AllShelves'
 
 class App extends React.Component {
 
@@ -23,56 +26,20 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-
-      {/*TODO sort this out with router*/}
-
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: Need to refer to SearchTerms.md
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) :
-
-        // MAIN SECTION
-
-        (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div>{this.props.books}</div>
-            <div className="list-books-content">
-              <div>
-                {/*TODO try to use super in the Bookshelf.js file to prevent passing props down*/}
-                {/*Shelf removed*/}
-                <Bookshelf books={this.state.books} category="currentlyReading" catTitle="Currently Reading" updateBooks={this.updateBooks}/>
-                {/*Shelf removed*/}
-                <Bookshelf books={this.state.books} category="wantToRead" catTitle="Want to Read" updateBooks={this.updateBooks}/>
-                {/*Shelf removed*/}
-                <Bookshelf books={this.state.books} category="read" catTitle="Read" updateBooks={this.updateBooks}/>
-              </div>
-            </div>
-
-            {/*TODO Need to modify this to use router rather than state*/}
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
+          <Route exact path='/' render={() => (
+            <AllShelves
+              books={this.state.books} updateBooks={this.updateBooks}
+            />
+          )}/>
+          <Route path='/search' render={() => (
+            <BookSearch
+              updateBooks={this.updateBooks}
+            />
+          )}/>
       </div>
     )
   }
+
 }
 
 export default App
