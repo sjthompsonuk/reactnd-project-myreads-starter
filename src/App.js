5c5c12cfa325844560ff67ae4c2d985f679e33f9
Sam Thompson <noreply@gmail.com>
2018-07-14 18:21:14 +0200

    Add API book retreival

--- src/App.js
+++ src/App.js
@@ -5,25 +5,36 @@ import './App.css'
 import Bookshelf from './Bookshelf'
 
 class App extends React.Component {
+
   state = {
-    /* Added React Router*/
+    books:[]
+  }
+
+  updateBooks = () => {
+    BooksAPI.getAll().then((books) =>{
+      this.setState({books})
+    })
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) =>{
+      this.setState({books:books})
+    })
   }
 
   render() {
     return (
       <div className="app">
+
+      {/*TODO sort this out with router*/}
+
         {this.state.showSearchPage ? (
           <div className="search-books">
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
                 {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
+                  NOTES: Need to refer to SearchTerms.md
                 */}
                 <input type="text" placeholder="Search by title or author"/>
 
@@ -33,7 +44,9 @@ class App extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
+        ) :
+
+        (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -48,6 +61,8 @@ class App extends React.Component {
                 <Bookshelf/>
               </div>
             </div>
+
+            {/*TODO Need to modify this to use router rather than state*/}
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
