5c567c28f56c7245404d031ee3cd413c20e74bdf
Sam Thompson <noreply@gmail.com>
2018-07-14 15:29:08 +0200

    Add node modules for dev, chnage comments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI"
 import './App.css'
 
-class BooksApp extends React.Component {
+class App extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    /* TODO Add React Router*/
   }
 
   render() {
@@ -203,4 +197,4 @@ class BooksApp extends React.Component {
   }
 }
 
-export default BooksApp
+export default App
